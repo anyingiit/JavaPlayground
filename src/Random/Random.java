a62fa04bbcc7d96ef6a3f7335ddd29209d5bd8f5
anyingiit <49945850+anyingiit@users.noreply.github.com>
2022-05-30 01:00:45 +0800

    优化: Random: generateRandoms

--- src/Random/Random.java
+++ src/Random/Random.java
@@ -5,7 +5,7 @@ public class Random {
         int[] result = new int[scope];
         java.util.Random random = new java.util.Random();
         for (int i = 0; i < result.length; i++) {
-            result[i] = random.nextInt(10000);
+            result[i] = random.nextInt(scope);
         }
         return result;
     }
